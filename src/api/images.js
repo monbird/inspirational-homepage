807da247f5af8321a83f10e9461bc67b5f31cbba
Monika Ptaszynska <mon.ptasz@gmail.com>
2022-06-06 10:45:23 +0100

    randomize pictures

--- src/api/images.js
+++ src/api/images.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const API_URL = `https://api.unsplash.com/search/photos?query=nature&page=1&per_page=10&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`;
+const API_URL = 'https://api.unsplash.com/search/photos';
 
 const getImage = async () => {
-    const response = await axios.get(API_URL);
+    const page = Math.floor(Math.random() * (10 - 1 + 1) + 1); // between 1-10 inclusive
+    const query = `?query=nature&page=${page}&per_page=10&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`;
+    const url = `${API_URL}${query}`;
+    const response = await axios.get(url);
     const imageUrls = response.data.results.map((image) => image.urls.regular);
 
     return {
